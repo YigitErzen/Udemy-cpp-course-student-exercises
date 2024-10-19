62cb8db98a281b2f0f64b22fafb003f2570d2ab4
Yigit Erzen <144549821+YigitErzen@users.noreply.github.com>
2024-10-19 15:38:41 +0300

    Add files via upload

---
+++ Student Exercise 1/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	float radius,area;
+    cout<<"enter a radius of a circle"<<endl;
+    cin>>radius;
+    area=3.1425*radius*radius;
+    cout<<"the area of the circle is "<<area;
+}
