cd38f5c0650f11b813fb7a5166cc3a05fd999049
Yigit Erzen <144549821+YigitErzen@users.noreply.github.com>
2024-12-04 15:33:40 +0300

    Add files via upload

---
+++ Lesson 89/Lesson 89/Lesson 89.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+using namespace std;
+int main()
+{
+	int sum = 0;
+	int A[] = { 4,8,6,9,5,2,7 };
+	
+	for (int x : A)
+	{
+		sum += x;
+	}
+	cout << sum;
+
+	return 0;
+
+
+
+
+
+
+
+
+}
