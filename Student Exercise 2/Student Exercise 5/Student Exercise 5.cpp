c18ce15d4ede867081877b4c7c41b58252814a1d
Yigit Erzen <144549821+YigitErzen@users.noreply.github.com>
2024-12-12 13:45:18 +0300

    Add files via upload

---
+++ Student Exercise 2/Student Exercise 5/Student Exercise 5.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num, sum = 0, average;
+    cout << "Enter the numbers of element" << endl;
+    cin >> num;
+    
+    int A[100];
+    
+
+    for (int i = 0;i < num;i++)
+    {
+        cout << i+1 << "." << " Enter the number" << endl;
+        cin >> A[i];
+    }
+    for (int i = 0;i < num;i++)
+    {
+        sum += A[i];
+    }
+    average = sum / num;
+    
+    cout << "Average is: " << average;
+
+    return 0;
+}
